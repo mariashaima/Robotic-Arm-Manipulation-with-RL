d783df4d06c6ae062ff12e1ebbb06e3c304dfa51
Maria Shaima Joy <162720492+mariashaima@users.noreply.github.com>
2026-05-05 04:30:30 +0200

    Add files via upload

--- td3robosuitdoor/networks.py
+++ td3robosuitdoor/networks.py
@@ -1,36 +1,80 @@
+import os
+import torch as T
+import torch.nn.functional as F
+import torch.nn as nn
+import torch.optim as optim
+from torch.distributions.normal import Normal
 import numpy as np
 
-class ReplayBuffer():
-    def __init__(self, max_size, input_shape, n_actions):
-        self.mem_size = max_size
-        self.mem_ctr = 0
-        self.state_memory = np.zeros((self.mem_size, *input_shape))
-        self.new_state_memory = np.zeros((self.mem_size, *input_shape))
-        self.action_memory = np.zeros((self.mem_size, n_actions))
-        self.reward_memory = np.zeros(self.mem_size)
-        self.terminal_memory = np.zeros(self.mem_size, dtype=bool)
+class CriticNetwork(nn.Module):
+    def __init__(self, input_dims, n_actions, fc1_dims=400, fc2_dims=300, name='critic', chkpt_dir='tmp/td3', learning_rate=10e-3):
+        super(CriticNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_td3')
 
-    def store_transition(self, state, action, reward, state_, done):
-        index = self.mem_ctr % self.mem_size
+        self.fc1 = nn.Linear(self.input_dims[0]+n_actions, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.q1 = nn.Linear(self.fc2_dims, 1)
 
-        self.state_memory[index] = state
-        self.new_state_memory[index] = state_
-        self.action_memory[index] = action
-        self.reward_memory[index] = reward
-        self.terminal_memory[index] = done
+        self.optimizer = optim.AdamW(self.parameters(), lr=learning_rate, weight_decay=0.005)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
 
-        self.mem_ctr += 1
+        self.to(self.device)
 
-    def sample_buffer(self, batch_size):
-        max_mem = min(self.mem_ctr, self.mem_size)
-        batch = np.random.choice(max_mem, batch_size)
+    def forward(self, state, action):
+        action_value = self.fc1(T.cat([state, action], dim=1))
+        action_value = F.relu(action_value)
+        action_value = self.fc2(action_value)
+        action_value = F.relu(action_value)
 
-        states = self.state_memory[batch]
-        states_ = self.new_state_memory[batch]
-        actions = self.action_memory[batch]
-        rewards = self.reward_memory[batch]
-        dones = self.terminal_memory[batch]
+        q1 = self.q1(action_value)
 
-        return states, actions, rewards, states_, dones
+        return q1
 
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
 
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
+
+class ActorNetwork(nn.Module):
+    def __init__(self, input_dims, fc1_dims=400, learning_rate=10e-3,
+                 fc2_dims=300, n_actions=2, name='actor', chkpt_dir='tmp/td3'):
+        super(ActorNetwork, self).__init__()
+        self.input_dims = input_dims
+        self.fc1_dims = fc1_dims
+        self.fc2_dims = fc2_dims
+        self.n_actions = n_actions
+        self.name = name
+        self.checkpoint_dir = chkpt_dir
+        self.checkpoint_file = os.path.join(self.checkpoint_dir, name+'_td3')
+
+        self.fc1 = nn.Linear(*self.input_dims, self.fc1_dims)
+        self.fc2 = nn.Linear(self.fc1_dims, self.fc2_dims)
+        self.output = nn.Linear(self.fc2_dims, self.n_actions)
+
+        self.optimizer = optim.Adam(self.parameters(), lr=learning_rate)
+        self.device = T.device('cuda:0' if T.cuda.is_available() else 'cpu')
+
+        self.to(self.device)
+
+    def forward(self, state):
+        x = self.fc1(state)
+        x = F.relu(x)
+        x = self.fc2(x)
+        x = F.relu(x)
+
+        x = T.tanh(self.output(x))
+
+        return x
+
+    def save_checkpoint(self):
+        T.save(self.state_dict(), self.checkpoint_file)
+
+    def load_checkpoint(self):
+        self.load_state_dict(T.load(self.checkpoint_file))
